0ad73dbb7616c08b96b57757442441409f6f1eb7
Ksawery Raupuk <85706573+Ksawere@users.noreply.github.com>
2025-03-23 15:35:12 +0100

    Update and rename insert.sql to insertT1.sql

---
+++ insertT1.sql
@@ -0,0 +1,57 @@
+
+--CUSTOMER ACCOUNT INFO
+BULK INSERT CustomerAccountInfo FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\customers_T1.bulk'
+WITH (
+   FIELDTERMINATOR = '|',
+   ROWTERMINATOR = '0x0a' 
+);
+SELECT *FROM CustomerAccountInfo;
+
+--OWNED BOARD GAMES 
+BULK INSERT OwnedBoardGames FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\owned_board_games.bulk'
+WITH (
+	FIELDTERMINATOR = '|',
+    ROWTERMINATOR = '0x0a' 
+);
+SELECT *FROM OwnedBoardGames;
+
+WORKERS
+
+BULK INSERT Workers
+FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\workers_T1.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    ROWTERMINATOR = '0x0a'
+);
+SELECT *FROM Workers;
+--TOURNAMENTS 
+BULK INSERT Tournaments FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\tournaments_T1.bulk'
+WITH (
+	FIELDTERMINATOR = '|',
+    ROWTERMINATOR = '0x0a' 
+	);
+SELECT *FROM Tournaments;
+
+--TOURNAMENT PARTICIPANTS 
+BULK INSERT TournamentParticipants FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\tournament_participants_T1.bulk'
+WITH (
+    FIELDTERMINATOR = '|',
+    ROWTERMINATOR = '0x0a' 
+	);
+SELECT *FROM TournamentParticipants;
+
+-- RENTS
+BULK INSERT Rents FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\rents_T1.bulk'
+WITH (
+   FIELDTERMINATOR = '|',
+   ROWTERMINATOR = '0x0a' 
+	);
+SELECT *FROM Rents;
+
+--POSTERS 
+BULK INSERT Posters FROM 'C:\Users\ksawery1\Desktop\datawarehouse_project\posters_T1.bulk'
+WITH (
+   FIELDTERMINATOR = '|',
+   ROWTERMINATOR = '0x0a' 
+	);
+SELECT *FROM Posters;
