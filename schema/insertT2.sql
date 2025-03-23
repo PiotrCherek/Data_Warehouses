c69e4b18560899115946e2762e9590035a92043a
PiotrCherek <piotrekch179@gmail.com>
2025-03-23 17:32:49 +0100

    final

--- schema/insertT2.sql
+++ schema/insertT2.sql
@@ -1,11 +1,10 @@
-
 --CUSTOMER ACCOUNT INFO
 BULK INSERT CustomerAccountInfo FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\customers_T2.bulk'
 WITH (
    FIELDTERMINATOR = '|',
    ROWTERMINATOR = '0x0a' 
 );
-SELECT *FROM CustomerAccountInfo;
+SELECT * FROM CustomerAccountInfo;
 
 --OWNED BOARD GAMES 
 BULK INSERT OwnedBoardGames FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\owned_board_games.bulk'
@@ -13,7 +12,7 @@ WITH (
 	FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
 );
-SELECT *FROM OwnedBoardGames;
+SELECT * FROM OwnedBoardGames;
 
 --WORKERS
 
@@ -23,7 +22,7 @@ WITH (
     FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a'
 );
-SELECT *FROM Workers;
+SELECT * FROM Workers;
 
 --TOURNAMENTS 
 BULK INSERT Tournaments FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\tournaments_T2.bulk'
@@ -31,7 +30,7 @@ WITH (
 	FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
 	);
-SELECT *FROM Tournaments;
+SELECT * FROM Tournaments;
 
 --TOURNAMENT PARTICIPANTS 
 BULK INSERT TournamentParticipants FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\tournament_participants_T2.bulk'
@@ -39,7 +38,7 @@ WITH (
     FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
 	);
-SELECT *FROM TournamentParticipants;
+SELECT * FROM TournamentParticipants;
 
 -- RENTS
 BULK INSERT Rents FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\rents_T2.bulk'
@@ -47,7 +46,7 @@ WITH (
    FIELDTERMINATOR = '|',
    ROWTERMINATOR = '0x0a' 
 	);
-SELECT *FROM Rents;
+SELECT * FROM Rents;
 
 --POSTERS 
 BULK INSERT Posters FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\posters_T2.bulk'
@@ -55,4 +54,9 @@ WITH (
    FIELDTERMINATOR = '|',
    ROWTERMINATOR = '0x0a' 
 	);
-SELECT *FROM Posters;
+SELECT * FROM Posters;
+
+UPDATE Posters
+SET Number_Of_Posters = 10
+WHERE Tournament_ID = 1;
+SELECT * FROM Posters;
