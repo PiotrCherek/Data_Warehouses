01dc4fd49d2cfddffd8ba1d1b2b4ac447aeed869
PiotrCherek <piotrekch179@gmail.com>
2025-04-16 10:28:32 +0200

    zmiany

--- insertT1.sql
+++ insertT1.sql
@@ -1,6 +1,5 @@
-
 --CUSTOMER ACCOUNT INFO
-BULK INSERT CustomerAccountInfo FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\customers_T1.bulk'
+BULK INSERT CustomerAccountInfo FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\customers_T1.bulk'
 WITH (
    FIELDTERMINATOR = '|',
    ROWTERMINATOR = '0x0a' 
@@ -8,7 +7,7 @@ WITH (
 SELECT *FROM CustomerAccountInfo;
 
 --OWNED BOARD GAMES 
-BULK INSERT OwnedBoardGames FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\owned_board_games_T1.bulk'
+BULK INSERT OwnedBoardGames FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\owned_board_games_T1.bulk'
 WITH (
 	FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
@@ -18,14 +17,14 @@ SELECT *FROM OwnedBoardGames;
 --WORKERS
 
 BULK INSERT Workers
-FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\workers_T1.bulk'
+FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\workers_T1.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a'
 );
 SELECT *FROM Workers;
 --TOURNAMENTS 
-BULK INSERT Tournaments FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\tournaments_T1.bulk'
+BULK INSERT Tournaments FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\tournaments_T1.bulk'
 WITH (
 	FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
@@ -33,7 +32,7 @@ WITH (
 SELECT *FROM Tournaments;
 
 --TOURNAMENT PARTICIPANTS 
-BULK INSERT TournamentParticipants FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\tournament_participants_T1.bulk'
+BULK INSERT TournamentParticipants FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\tournament_participants_T1.bulk'
 WITH (
     FIELDTERMINATOR = '|',
     ROWTERMINATOR = '0x0a' 
@@ -41,7 +40,7 @@ WITH (
 SELECT *FROM TournamentParticipants;
 
 -- RENTS
-BULK INSERT Rents FROM 'C:\Users\Piotrullo\Documents\GitHub\Data_Warehouses\rents_T1.bulk'
+BULK INSERT Rents FROM 'C:\Users\Piotrek\Documents\GitHub\Data_Warehouses\rents_T1.bulk'
 WITH (
    FIELDTERMINATOR = '|',
    ROWTERMINATOR = '0x0a' 
